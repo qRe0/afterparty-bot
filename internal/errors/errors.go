7b0a5685238bd64b9dfe41417788f9628fa19d1b
qre <melsenmalina@gmail.com>
2025-02-26 17:58:33 +0300

    Removed unused err vars

--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -4,6 +4,4 @@ import "github.com/pkg/errors"
 
 var (
 	ErrCheckingBaseParameters = errors.New("failed to check base parameters: something wrong with ")
-	ErrUpdatingGoogleSheet    = errors.New("failed to update google sheet with transactions")
-	ErrGeneratingTicketImg    = errors.New("failed to generate ticket image")
 )
