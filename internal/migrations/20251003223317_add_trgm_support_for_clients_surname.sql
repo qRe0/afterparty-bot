89bb0c8157c167dbf6e58bd9e1d3fc9246b5678d
qRe0 <melsenmalina@gmail.com>
2025-10-04 01:54:07 +0300

    hotfix: removed concurrent running of index creation

--- internal/migrations/20251003223317_add_trgm_support_for_clients_surname.sql
+++ internal/migrations/20251003223317_add_trgm_support_for_clients_surname.sql
@@ -1,12 +1,10 @@
--- +goose Transaction false
-
 -- +goose Up
 -- +goose StatementBegin
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
-CREATE INDEX CONCURRENTLY idx_clients_surname_gin_trgm ON tickets USING gin (surname gin_trgm_ops);
+CREATE INDEX idx_clients_surname_gin_trgm ON tickets USING gin (surname gin_trgm_ops);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-DROP INDEX CONCURRENTLY IF EXISTS idx_clients_surname_gin_trgm;
+DROP INDEX IF EXISTS idx_clients_surname_gin_trgm;
 -- +goose StatementEnd
